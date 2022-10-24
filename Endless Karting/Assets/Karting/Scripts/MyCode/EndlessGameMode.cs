f8b4be73ab0abdcdf0f4f47f081daf00127602c5
Bedheadhere <83078551+Bedheadhere@users.noreply.github.com>
2022-10-24 22:29:12 +0100

    track turn left added

--- Endless Karting/Assets/Karting/Scripts/MyCode/EndlessGameMode.cs
+++ Endless Karting/Assets/Karting/Scripts/MyCode/EndlessGameMode.cs
@@ -4,45 +4,95 @@ using UnityEngine;
 
 public class EndlessGameMode : MonoBehaviour
 {
-    public GameObject road;
+    public GameObject straightTrack;
+    public GameObject medTurnTrack;
 
-    Vector3 trackSpawnLocation = new Vector3(140, 2f, -50);
-    Vector3 trackOffset = new Vector3(0, 0, 0);
+    Vector3 trackSpawnLocation = new Vector3(140, 15f, -50);
+    Vector3 trackPositionOffset = new Vector3(0, 0, 0);
+    Vector3 trackRotationOffset = new Vector3(0, 0, 0);
 
-    public float roadDestroyTimer = 2.0f;
+    public float roadDestroyTimer = 1.0f;
 
-    // Start is called before the first frame update
+    public string trackDirection = "Front";
+
+
     void Start()
     {
-        InvokeRepeating("BuildRoad", 1.0f, 3.0f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        InvokeRepeating("BuildRoad", 1.0f, 2.0f);
     }
 
     private void BuildRoad()
     {
         StartCoroutine(StraightTrackPieces());
+        
     }
 
     private IEnumerator StraightTrackPieces()
     {
         for (int numberofStraightTracks = 0; numberofStraightTracks < 3; numberofStraightTracks++)
         {
-            InstantiateTrackPiece();
-            yield return new WaitForSeconds(0.5f);
+            TrackForward();
+            yield return new WaitForSeconds(0.1f);
+        }
+        TrackTurn();
+    }
+
+    private void TrackForward()
+    {
+        GameObject road = Instantiate(straightTrack, trackSpawnLocation + trackPositionOffset, Quaternion.Euler(trackRotationOffset));
+
+        Destroy(road, roadDestroyTimer);
+
+        switch (trackDirection)
+        {
+            case "Right":
+                trackPositionOffset = trackPositionOffset + new Vector3(10, 0, 0);
+                break;
+            case "Left":
+                trackPositionOffset = trackPositionOffset + new Vector3(-10, 0, 0);
+                break;
+            case "Back":
+                trackPositionOffset = trackPositionOffset + new Vector3(0, 0, -10);
+                break;
+            case "Front":
+                trackPositionOffset = trackPositionOffset + new Vector3(0, 0, 10);
+                break;
+            default:
+                trackPositionOffset = trackPositionOffset + new Vector3(0, 0, 10);
+                break;
         }
     }
 
-    private void InstantiateTrackPiece()
+    private void TrackTurn()
     {
-        GameObject dRoad = Instantiate(road, trackSpawnLocation + trackOffset, Quaternion.identity);
+        GameObject road = Instantiate(medTurnTrack, trackSpawnLocation + trackPositionOffset, Quaternion.Euler(trackRotationOffset));
 
-        Destroy(dRoad, roadDestroyTimer);
+        Destroy(road, roadDestroyTimer);
 
-        trackOffset = trackOffset + new Vector3(0, 0, 10);
+        trackRotationOffset = trackRotationOffset + new Vector3(0, -90, 0);
+
+        switch (trackDirection)
+        {
+            case "Right":
+                trackDirection = "Front";
+                trackPositionOffset = trackPositionOffset + new Vector3(10, 0, 10);
+                break;
+            case "Left":
+                trackDirection = "Back";
+                trackPositionOffset = trackPositionOffset + new Vector3(-10, 0, -10);
+                break;
+            case "Back":
+                trackDirection = "Right";
+                trackPositionOffset = trackPositionOffset + new Vector3(10, 0, -10);
+                break;
+            case "Front":
+                trackDirection = "Left";
+                trackPositionOffset = trackPositionOffset + new Vector3(-10, 0, 10);
+                break;
+            default:
+                trackDirection = "Left";
+                trackPositionOffset = trackPositionOffset + new Vector3(-10, 0, 10);
+                break;
+        }
     }
 }
